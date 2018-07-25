e0f4f75992b6e7f9058e88cffd2d9edb70fb2d6c
Scott McDonald <Scott.McDonald@dish.com>
2018-07-25 15:19:40 -0600

    math/pi

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@GetMapping("/")
-	public String helloWorld() {
-		return "Hello from Spring!";
-	}
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/math/pi")
+    public String pi() {
+        return String.valueOf(Math.PI);
+    }
 }
---
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testRootEndpoint() throws Exception {
+        mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello from Spring!"));
+    }
+
+    @Test
+    public void testMathPiEndpoint() throws Exception {
+        mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.valueOf(Math.PI)));
+    }
+}
